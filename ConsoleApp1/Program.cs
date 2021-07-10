5c93483440b31f50016f7bd2ab6dd7e60d8c59a4
marjangoodarzi@gmail.com <marjangoodarzi@gmail.com>
2021-07-10 13:34:44 +0430

    done

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using log4net;
+using log4net.Config;
+using System;
+using System.IO;
+using System.Reflection;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        private static readonly ILog log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+        static void Main(string[] args)
+        {
+
+            // Load configuration
+            var logRepository = LogManager.GetRepository(Assembly.GetEntryAssembly());
+            XmlConfigurator.Configure(logRepository, new FileInfo("log4net.config"));
+
+           
+            // Log some things
+            log.Info("Hello logging world!");
+            Console.WriteLine("Hello World2!");
+            log.Error("Error!");
+            log.Warn("Warn!");
+        }
+    }
+}
